e4c8ab3800cdfa4e76ff05634615e6cb309b9d67
Andre <42361690+AndreCalderonB@users.noreply.github.com>
2020-12-01 17:22:36 -0600

    Update lab.mk

--- city-traffic/lab.mk
+++ city-traffic/lab.mk
@@ -1,12 +1,6 @@
 # City-Traffic Simulator 2020 automation test
 
-build:
-	go get github.com/hajimehoshi/ebiten
-	go get github.com/hajimehoshi/ebiten/ebitenutil
-	go get golang.org/x/image/font/basicfont
-	go get github.com/AndreCalderonB/City_Traffic/scripts
-	go build main.go
-
-test: build
-	@echo Test City-Traffic - main.go
-	./main 5
+include ../../common.mk
+run:
+	go get -v github.com/hajimehoshi/ebiten
+	go run main.go 4
